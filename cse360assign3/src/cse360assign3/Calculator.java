2f4a9863827a4ea40fd5e9a529db8f9cea4043f0
Jaafar Al Lawati <jaafar.allawati@gmail.com>
2016-02-22 01:18:50 -0700

    Initial check in

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,86 @@
+/** Description of Calculator:
+*		This class is a simple calculator that add, subtract, multiply and divide
+*		This class also shows the history of the operation
+*
+* @author Jaafar Al Lawati
+* @version Feb 21, 2015.
+* Pin 102
+* 
+* 
+* 
+*/
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Sets total to 0 and adds 0 to the history string   
+	 * 
+	 * @param none
+	 */
+	public Calculator () {
+		total = 0; // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total value
+	 * 
+	 * @param none
+	 * @return	total		the total after the operations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the value to the total
+	 * 
+	 * @param value			the value that will be added to the total
+	 * @return	none		
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract the value from the total
+	 * 
+	 * @param value			the value that will be subtract from the total
+	 * @return	none		
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply's the total with the value
+	 * 
+	 * @param value			the value that will be multiplied with
+	 * @return	none		
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide's the total with the value
+	 * 
+	 * @param value			the value that will be divided with
+	 * @return	none		
+	 */
+	public void divide (int value) {
+		
+	}
+
+	/**
+	 * Returns the history of the operations
+	 * 
+	 * @param none			
+	 * @return	history		history of the operations		
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,81 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	
+	@Test
+	public void CalculatorTest(){
+		fail("Not yet implemented");
+		Calculator calc1 = new Calculator();
+		assertNotNull(calc1);
+		
+	}
+	
+	@Test
+	public void getTotalTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		assertEquals(10, calc1.getTotal());
+	}
+	
+	@Test
+	public void addTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		assertEquals(10, calc1.getTotal());
+	}
+	
+	@Test
+	public void subtractTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		calc1.subtract(1);
+		assertEquals(9, calc1.getTotal());
+	}
+	
+	@Test
+	public void multiplyTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		calc1.subtract(1);
+		calc1.multiply(2);
+		assertEquals(18, calc1.getTotal());
+	}
+	
+	@Test
+	public void divideTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		calc1.subtract(1);
+		calc1.multiply(2);
+		calc1.divide(2);
+		assertEquals(9, calc1.getTotal());
+	}
+	
+	@Test
+	public void divideTest2(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		calc1.subtract(1);
+		calc1.multiply(2);
+		calc1.divide(0);
+		assertEquals(0, calc1.getTotal());
+	}
+	@Test
+	public void getHistoryTest(){
+		Calculator calc1 = new Calculator();
+		calc1.add(10);
+		calc1.subtract(1);
+		calc1.multiply(2);
+		calc1.divide(2);
+		assertEquals(9, calc1.getTotal());
+		assertEquals("0 + 10 - 1 * 2 / 2", calc1.getHistory());
+	}
+	
+	
+
+}
